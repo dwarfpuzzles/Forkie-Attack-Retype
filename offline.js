1eb1a4da074ca9fc4e96813fb195d49b55492007
cab file <32414797+cabfile@users.noreply.github.com>
2021-11-26 13:32:43 +0300

    in case you wanna play it already

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1637922609,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"data/sprite-sheet0.png",
+		"data/particles.png",
+		"data/sprite2-sheet0.png",
+		"data/powerup-sheet0.png",
+		"data/particles2.png",
+		"data/camera-sheet0.png",
+		"data/sprite3-sheet0.png",
+		"data/sprite3-sheet1.png",
+		"data/sprite3-sheet2.png",
+		"data/sprite4-sheet0.png",
+		"data/sprite5-sheet0.png",
+		"data/vine-boomfull.ogg",
+		"data/chord.ogg",
+		"data/exp.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
